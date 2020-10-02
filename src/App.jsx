f4427dafee299c4006e965dba20fca6e76fb38d9
Macbook <trieungochai.dev@gmail.com>
2020-10-02 18:32:27 +0700

    Commit code base

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,23 @@
 import React from 'react';
+import './App.css';
 
 class App extends React.Component {
   render() {
     return (
-      <div>Music Master</div>
+      <div className='App'>
+        <div className='App-title'>Music Master</div>
+        <div>
+          <input placeholder='Search for Artist'/>
+          <button type='button' className="glyphicon glyphicon-search"/>
+        </div>
+        <div className='Profile'>
+          <div>Artist Picture</div>
+          <div>Artist Name</div>
+        </div>
+        <div className='Gallery'>
+          Gallery
+        </div>
+      </div>
     );
   };
 };
