4ef038fe35edabd2d0d8a56456873f7b0913e678
Macbook <trieungochai.dev@gmail.com>
2020-10-05 10:32:21 +0700

    Creating a search funtion to help users searching for Artists

--- src/App.jsx
+++ src/App.jsx
@@ -2,13 +2,39 @@ import React from 'react';
 import './App.css';
 
 class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      query: '',
+    };
+  };
+
+  search = () => {
+    console.log('this.state', this.state);
+  };
+
   render() {
     return (
       <div className='App'>
         <div className='App-title'>Music Master</div>
         <div>
-          <input placeholder='Search for Artist'/>
-          <button type='button' className="glyphicon glyphicon-search"/>
+          <input
+            type='text'
+            placeholder='Search for Artist'
+            value={this.state.query}
+            onChange={(event) => {this.setState({query: event.target.value})}}
+            onKeyPress={(event) => {
+              if (event.key === 'Enter') {
+                this.search();
+              }
+            }}
+            style={{color: "black"}}
+            />
+          <button
+            type='button'
+            className="glyphicon glyphicon-search"
+            onClick={this.search}
+            style={{color: "black"}}/>
         </div>
         <div className='Profile'>
           <div>Artist Picture</div>
